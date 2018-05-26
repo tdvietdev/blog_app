75e2f60417daf0824cacea3319a80696d3eafbb4
Trinh Dinh Viet <tdviet.dev@gmail.com>
2018-05-26 07:09:40 +0700

    finish i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  before_action :set_locale
 
+  private
+  def set_locale
+    I18n.locale = session[:locale] || I18n.default_locale
+    session[:locale] = I18n.locale
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'set_language/en'
+  get 'set_language/vi'
   get "sessions/new"
   root "static_pages#home"
   get  "/help",    to: "static_pages#help"
